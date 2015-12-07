e713e59060bf76a9f3d28e66ec27b382c9207159
zdkswd <2822464407@qq.com>
2015-12-07 22:08:53 +0800

    hello world

---
+++ app/src/main/java/com/walkeasily/cs2015/walkeasily/MainActivity.java
@@ -0,0 +1,59 @@
+package com.walkeasily.cs2015.walkeasily;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BaiduMapOptions;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.model.LatLng;
+
+
+public class MainActivity extends Activity {
+
+
+
+    private MapView mMapView = null;
+    private BaiduMap mBaiduMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+
+    private void init() {
+        mMapView = (MapView) findViewById(R.id.bmapview);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+    @Override
+    protected void onDestroy() {
+        mMapView.onDestroy();
+        mMapView = null;
+        super.onDestroy();
+    }
+
+
+
+
+}
