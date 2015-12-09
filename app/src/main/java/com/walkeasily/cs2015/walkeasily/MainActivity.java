813b6b732b99432905c299a3468c43e82fc11c01
zdkswd <2822464407@qq.com>
2015-12-09 11:58:24 +0800

    1234

--- app/src/main/java/com/walkeasily/cs2015/walkeasily/MainActivity.java
+++ app/src/main/java/com/walkeasily/cs2015/walkeasily/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        //
+
         SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
         init();
