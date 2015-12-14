d7777f9129aabca6788f743e197241583a9a9397
zdkswd <2822464407@qq.com>
2015-12-14 22:46:40 +0800

    1234

--- app/src/main/java/com/walkeasily/cs2015/walkeasily/MainActivity.java
+++ app/src/main/java/com/walkeasily/cs2015/walkeasily/MainActivity.java
@@ -2,37 +2,63 @@ package com.walkeasily.cs2015.walkeasily;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
+import android.widget.Toast;
 
+import com.baidu.location.BDLocation;
+
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.location.Poi;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BaiduMapOptions;
 import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.model.LatLng;
+
+import java.util.List;
 
 
 public class MainActivity extends Activity {
 
 
-
     private MapView mMapView = null;
     private BaiduMap mBaiduMap;
+    public LocationClient mLocationClient = null;
+    public BDLocationListener myListener = new MyLocationListener();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-
         SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
         init();
+
+
     }
 
 
     private void init() {
+
         mMapView = (MapView) findViewById(R.id.bmapview);
+        mBaiduMap=mMapView.getMap();
+        mMapView.showZoomControls(false);
+        mLocationClient = new LocationClient(getApplicationContext());     //声明LocationClient类
+        mLocationClient.registerLocationListener(myListener);    //注册监听函数
+        initLocation();
+        mLocationClient.start();
+
+
     }
 
 
@@ -41,11 +67,13 @@ public class MainActivity extends Activity {
         super.onResume();
         mMapView.onResume();
     }
+
     @Override
     protected void onPause() {
         super.onPause();
         mMapView.onPause();
     }
+
     @Override
     protected void onDestroy() {
         mMapView.onDestroy();
@@ -53,7 +81,48 @@ public class MainActivity extends Activity {
         super.onDestroy();
     }
 
+    public class MyLocationListener implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //Receive Location
+            double x=location.getLatitude();
+            double y=location.getLongitude();
+            LatLng cenpt =  new LatLng(x,y);
+//定义地图状态
+            MapStatus mMapStatus = new MapStatus.Builder()
+                    .target(cenpt)
+                    .zoom(18)
+                    .build();
+//定义MapStatusUpdate对象，以便描述地图状态将要发生的变化
+
+            MapStatusUpdate mMapStatusUpdate = MapStatusUpdateFactory.newMapStatus(mMapStatus);
+//改变地图状态
+            mBaiduMap.setMapStatus(mMapStatusUpdate);
+        }
 
 
 
+
+    }
+
+
+
+    private void initLocation(){
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy
+        );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
+        int span=1000;
+        option.setScanSpan(0);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true);//可选，默认false,设置是否使用gps
+        option.setLocationNotify(true);//可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
+        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        option.setIgnoreKillProcess(false);//可选，默认false，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认杀死
+        option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+        option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤gps仿真结果，默认需要
+        mLocationClient.setLocOption(option);
+    }
 }
